d6c78b3cb99746cc546f3efcdbe698010d8e4f03
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2024-12-08 11:50:57 +0300

    updates on port

--- Server.js
+++ Server.js
@@ -60,7 +60,7 @@ app.delete('/users/:id', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4005;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
