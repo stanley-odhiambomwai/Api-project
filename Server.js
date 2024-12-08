891b719beb65f9bb8bb3e5e975957525b26f33f8
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2024-12-08 10:03:31 +0300

    Setting the server

--- Server.js
+++ Server.js
@@ -59,3 +59,8 @@ app.delete('/users/:id', async (req, res) => {
         res.status(400).send('Error deleting user');
     }
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
