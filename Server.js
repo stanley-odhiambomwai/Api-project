4ee4a74fd82c741763bafd836421c4754f284c1b
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2024-12-08 09:52:48 +0300

    Import the User model

--- Server.js
+++ Server.js
@@ -11,3 +11,6 @@ app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch((error) => console.log('Failed to connect to MongoDB:', error));
+
+    
+const User = require('./models/user.js');
