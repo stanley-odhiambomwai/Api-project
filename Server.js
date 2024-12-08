766237334f086fcab83b1f266e067c5d1b851fc5
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2024-12-08 09:50:59 +0300

    Cnnecting to mongoDB

--- Server.js
+++ Server.js
@@ -7,3 +7,7 @@ const app = express();
 
 
 app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.log('Failed to connect to MongoDB:', error));
